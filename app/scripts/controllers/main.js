9fe7040aa530a0983eed98f9204205be121b379b
Liz Abinante <eabinante@gmail.com>
2014-08-28 20:44:17 -0500

    repeats for charts, remainder of form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,4 +10,5 @@
 angular.module('travelingWomanApp')
   .controller('MainCtrl', function ($scope) {
     $scope.repeats = [7, 9, 11, 13, 15, 17, 19, 21];
+    $scope.charts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   });
